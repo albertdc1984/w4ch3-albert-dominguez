309997bb88dd33d1265f19c2c889706119d5dbc9
Albert Domínguez <albertdc1984@gmail.com>
2022-02-02 23:22:02 +0100

    Buttons add numbers to an array

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -1,14 +1,26 @@
+import Display from "../Display/Display";
 import PhoneNumber from "../PhoneNumber/PhoneNumber";
+
 const Actions = () => {
+  const callFunction = () => {
+    <Display className={`message hidden`} />;
+  };
+  const hangFunction = () => {
+    <Display className={`message`} />;
+  };
   return (
     <>
       <div className="actions">
         <PhoneNumber />
         {/* <!-- El botón de llamar debe tener la clase "activo" cuando -->
           <!-- el número de teléfono tiene 9 cifras --> */}
-        <a className="call">Call</a>
+        <a href="call" className="call" onClick={callFunction}>
+          Call
+        </a>
         {/*  <!-- Sólo se tiene que ver un botón u otro --> */}
-        <a className="hang active">Hang</a>
+        <a href="hang" className="hang active" onClick={hangFunction}>
+          Hang
+        </a>
       </div>
     </>
   );
