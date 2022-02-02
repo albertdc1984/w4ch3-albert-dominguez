ad299ce0d9718348936adfa8a70869d813c67329
Albert Domínguez <albertdc1984@gmail.com>
2022-02-02 19:54:20 +0100

    Deleted most of html in App.js and introduced separately in each component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
+import Actions from "./components/Actions/Actions";
+import Display from "./components/Display/Display";
+import Keyboard from "./components/Keyboard/Keyboard";
+
 function App() {
   return (
     <>
       <div className="container">
         {/* <!-- El siguiente elemento se oculta añadiéndole la clase "off" --> */}
-        <span className="message">Calling...</span>
+        <Display />
         <main className="phone">
           <div className="keyboard-container">
-            <ol className="keyboard">
-              <li>
-                <button className="key">1</button>
-              </li>
-              <li>
-                <button className="key">2</button>
-              </li>
-              <li>
-                <button className="key">3</button>
-              </li>
-              <li>
-                <button className="key">4</button>
-              </li>
-              <li>
-                <button className="key">5</button>
-              </li>
-              <li>
-                <button className="key">6</button>
-              </li>
-              <li>
-                <button className="key">7</button>
-              </li>
-              <li>
-                <button className="key">8</button>
-              </li>
-              <li>
-                <button className="key">9</button>
-              </li>
-              <li>
-                <button className="key">0</button>
-              </li>
-              <li>
-                <button className="key big">delete</button>
-              </li>
-            </ol>
+            <Keyboard />
           </div>
-          <div className="actions">
-            <span className="number">667359961</span>
-            {/* <!-- El botón de llamar debe tener la clase "activo" cuando -->
-          <!-- el número de teléfono tiene 9 cifras --> */}
-            <a href className="call">
-              Call
-            </a>
-            {/*  <!-- Sólo se tiene que ver un botón u otro --> */}
-            <a href className="hang active">
-              Hang
-            </a>
-          </div>
+          <Actions />
         </main>
       </div>
     </>
